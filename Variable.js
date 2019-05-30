2ff83459689a158c467156737f489e2bcf2beafa
angmayor <51034211+angmayor@users.noreply.github.com>
2019-05-30 09:42:07 -0400

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,5 @@
+var NombreYApellidos = "Angel Machado";
+var Edad = 30;
+var DiasDeLaSemana = "Lunes, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo";
+var ValorDeLaGravedad = "9,807 m/s²";
+var NumerosImpares = "1, 3, 5";
